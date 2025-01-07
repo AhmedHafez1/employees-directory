8d4c58892853aef97e186e60a95af4090a9e5e44
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2025-01-07 20:33:45 +0200

    Security Config with custom user and role tables

--- src/main/java/com/ghaza/tech/employees_directory/security/SecurityConfiguration.java
+++ src/main/java/com/ghaza/tech/employees_directory/security/SecurityConfiguration.java
@@ -17,7 +17,15 @@ public class SecurityConfiguration {
 
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // Query to get user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id = ?");
+
+        // Query to get user roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id = ?");
+
+        return jdbcUserDetailsManager;
     }
 
     // Override http filter
