62f4aecf9449404b76a32d0e60689fe3d50a5e46
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2025-01-07 16:59:28 +0200

    Security with JDBC

--- src/main/java/com/ghaza/tech/employees_directory/security/SecurityConfiguration.java
+++ src/main/java/com/ghaza/tech/employees_directory/security/SecurityConfiguration.java
@@ -2,32 +2,37 @@ package com.ghaza.tech.employees_directory.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        var omar = User.builder()
-                .username("omar")
-                .password("{noop}password")
-                .roles("USER", "MANAGER", "ADMIN")
-                .build();
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
-        var mariam = User.builder()
-                .username("mariam")
-                .password("{noop}password")
-                .roles("USER", "MANAGER")
-                .build();
+    // Override http filter
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((configurer) -> configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
 
-        var yosuf = User.builder()
-                .username("yosuf")
-                .password("{noop}password")
-                .roles("USER")
-                .build();
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
 
-        return new InMemoryUserDetailsManager(omar, mariam, yosuf);
+        return http.build();
     }
 }
